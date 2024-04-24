cc1e9da734da7e58753a8c94e67f23aa793b3aa2
ArsPro1323 <67059479+ArsPro13@users.noreply.github.com>
2024-04-24 14:34:18 +0300

    add lib

---
+++ pick_up_recipe/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('aaa'),
+        ),
+      ),
+    );
+  }
+}
