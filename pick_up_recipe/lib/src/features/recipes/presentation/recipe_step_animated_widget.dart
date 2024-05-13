d48b0e96cb8e6fdeb7f0ccfd494fd928a0b7af6d
ArsPro1323 <67059479+ArsPro13@users.noreply.github.com>
2024-05-13 14:29:45 +0300

    redesign

--- pick_up_recipe/lib/src/features/recipes/presentation/recipe_step_animated_widget.dart
+++ pick_up_recipe/lib/src/features/recipes/presentation/recipe_step_animated_widget.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:pick_up_recipe/src/features/recipes/domain/models/recipe_step_model.dart';
 
 class RecipeStepAnimatedWidget extends StatefulWidget {
@@ -22,10 +23,12 @@ class RecipeStepAnimatedWidget extends StatefulWidget {
 class _RecipeStepAnimatedWidgetState extends State<RecipeStepAnimatedWidget> {
   @override
   Widget build(BuildContext context) {
+    bool has_finished = (widget.sec >= widget.step.stop);
+
     double progress = (widget.sec - widget.step.start) /
         (widget.step.stop - widget.step.start);
     progress = min(1, max(0, progress));
-    
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -37,34 +40,41 @@ class _RecipeStepAnimatedWidgetState extends State<RecipeStepAnimatedWidget> {
       margin: const EdgeInsets.all(20),
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Этап: ${widget.step.instruction}'),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Text('Количество воды: ${widget.step.water} мл'),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Text('Длительность: ${widget.step.stop - widget.step.start} сек.'),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Text('Прошло: ${(progress * (widget.step.stop - widget.step.start)).round()} сек.'),
+            Column(
+              children: [
+                Text('Этап: ${widget.step.instruction}'),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text('Количество воды: ${widget.step.water} мл'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                      'Длительность: ${widget.step.stop - widget.step.start} сек.'),
+                ),
+              ],
             ),
             Container(
-              margin: const EdgeInsets.only(top: 20, bottom: 0),
-              width: 350,
-              height: 15,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                child: LinearProgressIndicator(
-                  value: progress,
-                  valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.primary),
+              width: 120,
+              height: 120,
+                child: CircularPercentIndicator(
+                  radius: 60,
+                  // value: progress,
+                  // strokeWidth: 10,
+                  percent: progress,
+                  lineWidth: has_finished ? 10 : 8,
+                  center: Text('${(progress * (widget.step.stop - widget.step.start) * 10).round() / 10} сек.'),
+                  progressColor: (
+                      has_finished
+                          ? Theme.of(context).colorScheme.outline
+                          : Theme.of(context).colorScheme.primary),
                   backgroundColor: const Color.fromARGB(255, 210, 210, 210),
-                  semanticsLabel: 'Progress indicator',
+                  // semanticsLabel: 'Progress indicator',
                 ),
-              ),
+
             ),
           ],
         ),
--- pick_up_recipe/lib/src/themes/light_theme.dart
+++ pick_up_recipe/lib/src/themes/light_theme.dart
@@ -4,5 +4,6 @@ ThemeData lightTheme = ThemeData(
   colorScheme: const ColorScheme.light(
     primary: Color.fromARGB(255, 1, 1, 159),
     secondary: Color.fromARGB(255, 32, 36, 45),
+    outline: Color.fromARGB(255, 0, 147, 42),
   ),
 );
