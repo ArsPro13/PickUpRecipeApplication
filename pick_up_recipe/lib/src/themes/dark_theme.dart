01b92f4d354fc1315d2ea5df6c5d901f32758cd4
ArsPro1323 <67059479+ArsPro13@users.noreply.github.com>
2024-07-02 11:04:15 +0300

    Revert "fix ignore, add class for app theme, add light/dark themes constraints"
    This reverts commit 9a1726c92278ee4917af914e7289fb747a32dba6.

---
+++ pick_up_recipe/lib/src/themes/dark_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: const ColorScheme.dark(
+    brightness: Brightness.dark,
+    primary: Color.fromARGB(255, 37, 37, 255),
+    secondary: Color.fromARGB(255, 166, 171, 171),
+    outline: Color.fromARGB(255, 0, 147, 42),
+    onBackground: Color.fromARGB(255, 27, 27, 31),
+    background: Color.fromARGB(255, 0, 0, 0),
+  ),
+);
