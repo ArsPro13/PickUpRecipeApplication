d4b3acd4924bf10d44f22f8f0a11ed0c8537b34d
avshink <andre.shinkarenko.04@mail.ru>
2026-08-13 22:45:41 +0300

    chore(router): маршруты базового рецепта и своего типа шага

--- pick_up_recipe/lib/routing/app_router.dart
+++ pick_up_recipe/lib/routing/app_router.dart
@@ -19,6 +19,8 @@ import '../src/pages/packs_page.dart';
 import '../src/pages/password_reset_page.dart';
 import '../src/pages/profile_page.dart';
 import '../src/pages/rating_page.dart';
+import '../src/pages/recipe_base_page.dart';
+import '../src/pages/custom_step_page.dart';
 import '../src/pages/recipe_builder_page.dart';
 import '../src/pages/recipes_for_coffee_page.dart';
 import '../src/pages/recipes_page.dart';
@@ -59,6 +61,8 @@ class AppRouter extends _$AppRouter {
 
         AutoRoute(page: RecipesForCoffeeRoute.page, path: '/coffee/methods', guards: [AuthGuard(ref)]),
         AutoRoute(page: GrinderSelectRoute.page, path: '/grinder', guards: [AuthGuard(ref)]),
+        AutoRoute(page: RecipeBaseRoute.page, path: '/recipe/base', guards: [AuthGuard(ref)]),
+        AutoRoute(page: CustomStepRoute.page, path: '/recipe/custom-step', guards: [AuthGuard(ref)]),
         AutoRoute(page: BrewRoute.page, path: '/brew', guards: [AuthGuard(ref)]),
         AutoRoute(page: RatingRoute.page, path: '/rating', guards: [AuthGuard(ref)]),
         AutoRoute(page: CorrectionReviewRoute.page, path: '/correction', guards: [AuthGuard(ref)]),
